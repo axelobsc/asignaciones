b5ec39226ef1154235f829929eb2041387c57f13
Axel Obscura <axel.obscura@derevo.com.mx>
2023-01-25 09:54:40 -0600

    asis

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.username,
-    password: process.env.password,
-    database: process.env.database
+    host: '217.21.77.51',
+    user: 'u304720237_derevo',
+    password: 'D2r2v4@123',
+    database: 'u304720237_derevo',
 });
 dbConn.connect(function(err) {
   if (err) throw err;
